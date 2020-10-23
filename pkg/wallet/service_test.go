3c92b8b4bf844f305184435dce7d8a5b2d026206
student <vyacheslavs453@gmail.com>
2020-10-23 15:44:24 +0500

    hw1 lk19

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -3,6 +3,7 @@ package wallet
 
 
 import (
+	"log"
 	"github.com/slayv1/wallet/pkg/types"
 	"testing"
 )
@@ -294,15 +295,32 @@ func BenchmarkSumPayment_user(b *testing.B){
 	}
 
 }
-func TestSumPaymentsWithProgress_empty(t *testing.T){
-    wallet := Service{};
-    var total types.Money = 0;
-    ch := wallet.SumPaymentsWithProgress();
-    var sum types.Money = 0;
-    for channel := range(ch){
-        sum += channel.Result;
-    }
-    if(total != sum){
-        t.Error("TestSumPaymentsWithProgress(): total is not equal to sum");
-    }
-}
+func BenchmarkSumPaymentsWithProgress_user(b *testing.B) {
+	var svc Service
+  
+	account, err := svc.RegisterAccount("+992000000001")
+  
+	if err != nil {
+	  b.Errorf("method RegisterAccount returned not nil error, account => %v", account)
+	}
+  
+	err = svc.Deposit(account.ID, 10000000_0000000)
+	if err != nil {
+	  b.Errorf("method Deposit returned not nil error, error => %v", err)
+	}
+  
+	for i := 0; i < 1000000000000; i++ {
+	  svc.Pay(account.ID, types.Money(i), "Cafe")
+	} 
+  
+	ch := svc.SumPaymentsWithProgress()
+  
+	 s, ok := <-ch
+  
+	if !ok {
+	  b.Errorf(" method SumPaymentsWithProgress ok not closed => %v", ok)
+	} 
+  
+	log.Println("=======>>>>>",s) 
+  
+  }
